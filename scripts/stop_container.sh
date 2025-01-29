35167544071dbba67b279de06398ae73c563d533
Naveen Shetty <naveen@creaza.com>
2025-01-30 00:09:07 +0100

    update stop script

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,10 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+# shellcheck disable=SC2016
+# shellcheck disable=SC1068
+containerID = docker ps | awk-F " " '{print $1}'
+
+# shellcheck disable=SC2154
+docker rm -f "$containerID"
+
