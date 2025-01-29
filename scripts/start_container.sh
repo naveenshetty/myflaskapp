026810679aa97190283f08aeeeb6b1668a87b653
Naveen Shetty <naveen@creaza.com>
2025-01-29 23:04:05 +0100

    updated start script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull naveen192/myflaskapp:tagname
+docker pull naveen192/myflaskapp:latest
 
 # Run the Docker image as a container
-docker run -d -p 8080:8080 naveen192/myflaskapp:tagname
+docker run -d -p 8080:8080 naveen192/myflaskapp:latest
